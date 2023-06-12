4ef850f37c0b01b2cb2df81f5ab5ae62b6e0ee41
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2023-06-12 08:54:57 -0300

    Use list comprehension for calculate new age

---
+++ 01 - List Comprehension.py
@@ -0,0 +1,10 @@
+idades = [20, 39, 18, 27, 19]
+
+print(idades)
+
+idades_ano_que_vem = [(idade+1)for idade in idades]
+print(idades_ano_que_vem)
+
+var = [(idade) for idade in idades if idade > 21]
+print(var)
+
