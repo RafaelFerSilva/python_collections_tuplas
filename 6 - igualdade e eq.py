82e93cb55720949b767505b4a31103a9f94baa23
Rafael Fernandes da Silva <81975862+Rafael-Orbia@users.noreply.github.com>
2023-07-10 18:35:12 -0300

    a igualdade com == retorna se um objeto tem o mesmo endereço de memória, já o __eq__ deve ser implementação conforme a necessidade da aplicação

---
+++ 6 - igualdade e eq.py
@@ -0,0 +1,70 @@
+import array as arr
+import numpy as np
+from abc import ABCMeta, abstractmethod
+
+
+class Conta(metaclass=ABCMeta):
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
+
+    def __str__(self):
+        return "[>> Código {} saldo {} <<]".format(self._codigo, self._saldo)
+
+
+class ContaCorrente(Conta):
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+
+class ContaPoupanca(Conta):
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+
+class ContaSalario:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def __eq__(self, outro):
+        if type(outro) != ContaSalario:
+            return False
+
+        return self._codigo == outro._codigo and self._saldo == outro._saldo
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return "[>> Código {} saldo {} <<]".format(self._codigo, self._saldo)
+
+
+class MultiploSalario(ContaSalario):
+    pass
+
+
+conta1 = ContaSalario(37)
+print(conta1)
+
+conta2 = ContaCorrente(37)
+print(conta2)
+
+# conta1.deposita(1)
+
+print(conta1 == conta2)
+
+print(isinstance(ContaCorrente(34), ContaSalario))
+
+
+
+
